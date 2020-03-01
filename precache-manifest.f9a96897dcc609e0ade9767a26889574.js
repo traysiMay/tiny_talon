6c02a73897a7de874fca7845f4142f450bf1d4a1
aklevecz <aklevecz@meowwolf.com>
2020-03-01 12:45:20 -0700

    Updates

---
+++ precache-manifest.f9a96897dcc609e0ade9767a26889574.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb9aa706b632653ef8019fb2a8fae31",
+    "url": "/tiny_talon/index.html"
+  },
+  {
+    "revision": "1b3c7502f52a0d2c4fba",
+    "url": "/tiny_talon/static/css/main.8d88dfd9.chunk.css"
+  },
+  {
+    "revision": "b8dd3e477754ad04f999",
+    "url": "/tiny_talon/static/js/2.6b4c7cb1.chunk.js"
+  },
+  {
+    "revision": "26967cad1b155fa9164333d88e27e117",
+    "url": "/tiny_talon/static/js/2.6b4c7cb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b3c7502f52a0d2c4fba",
+    "url": "/tiny_talon/static/js/main.f21835f5.chunk.js"
+  },
+  {
+    "revision": "b75106c08ef663ff0961",
+    "url": "/tiny_talon/static/js/runtime-main.10dd6dda.js"
+  },
+  {
+    "revision": "11fb02bfa4d794f4322c2a5a4b9dda0d",
+    "url": "/tiny_talon/static/media/Euclid.11fb02bf.ttf"
+  }
+]);
